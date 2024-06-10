5bce989ba40983844fe99e64c87962c249736f03
Movash <andrushhenko97@gmail.com>
2024-06-11 00:30:36 +0300

    add_fetch_photos

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { getPhotos } from "./src/api/photos.api";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getPhotos();
+        if (!data.length) {
+          Alert.alert("Error", "No photos");
+          return;
+        }
+        console.log("data", data);
+        setPhotos(data);
+      } catch (err) {
+        Alert.alert("Error", "Something went wrong");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPhotos();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {photos.map((obj) => (
+        <Text key={obj.id}>{obj.slug}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
