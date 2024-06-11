ba8c43e2b11db92b8ccf40c37bc4c8dfd4513c24
Movash <andrushhenko97@gmail.com>
2024-06-11 21:19:30 +0300

    add_show_photos

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, Image, View } from "react-native";
 import { getPhotos } from "./src/api/photos.api";
 import { useEffect, useState } from "react";
+import styled from "styled-components/native";
+
+const Post = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PostImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+`;
 
 export default function App() {
   const [photos, setPhotos] = useState([]);
@@ -28,21 +42,22 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <Post>
       <Text>Open up App.js to start working on your app!</Text>
       {photos.map((obj) => (
-        <Text key={obj.id}>{obj.slug}</Text>
+        // <Text key={obj.id}>{obj.slug}</Text>
+        <PostImage source={{ uri: obj.urls.raw }} key={obj.id}/>
       ))}
       <StatusBar style="auto" />
-    </View>
+    </Post>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
