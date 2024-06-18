3cae8b786eed984e914987765cad8edc052f3789
Movash <andrushhenko97@gmail.com>
2024-06-18 21:13:14 +0300

    relocate_content

--- App.js
+++ App.js
@@ -1,75 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Alert,
-  StyleSheet,
-  Text,
-  Image,
-  View,
-  FlatList,
-  ActivityIndicator,
-  RefreshControl,
-} from "react-native";
-import { getPhotos } from "./src/api/photos.api";
-import { useEffect, useState } from "react";
 import styled from "styled-components/native";
+import HomeScreen from './src/screens/HomeScreen';
 
 const Cont = styled.View`
   flex: 1;
   background-color: #fff;
   align-items: center;
   justify-content: center;
-`;
-
-const PostImage = styled.Image`
-  width: 100px;
-  height: 100px;
-  border-radius: 10px;
-  margin-top: 5px;
+  padding: 10px;
+  padding-top: 40px;
 `;
 
 export default function App() {
-  const [photos, setPhotos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchPhotos = async () => {
-    try {
-      setIsLoading(true);
-      const data = await getPhotos();
-      if (!data.length) {
-        Alert.alert("Error", "No photos");
-        return;
-      }
-      setPhotos(data);
-    } catch (err) {
-      Alert.alert("Error", "Something went wrong");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchPhotos();
-  }, []);
-
-  if (isLoading) {
-    return (
-      <Cont>
-        <ActivityIndicator size="large" />
-        <Text>Loading...</Text>
-      </Cont>
-    );
-  }
 
   return (
     <Cont>
-      <Text>Open up App.js to start working on your app!</Text>
-      <FlatList
-        refreshControl={
-          <RefreshControl refreshing={isLoading} onRefresh={fetchPhotos} />
-        }
-        data={photos}
-        renderItem={({ item }) => <PostImage source={{ uri: item.urls.raw }} />}
-      />
+      <HomeScreen />
       <StatusBar style="auto" />
     </Cont>
   );
